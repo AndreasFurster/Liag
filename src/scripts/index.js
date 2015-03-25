22ae7fe10493960f0578ad3b9d42f0adca2cf0d6
AndreasFurster <andreas.furster@hotmail.com>
2015-03-25 21:45:09 +0100

    Removed old code from tutorial

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -23,11 +23,3 @@
         // TODO: This application has been reactivated. Restore application state here.
     };
 } )();
-
-//(function () {
-//    'use strict';
-//    angular.module('xPlat', ['xPlat.services', 'xPlat.controllers', 'xPlat.directives']);
-//    angular.module('xPlat.directives', []);
-//    angular.module('xPlat.controllers', []);
-//    angular.module('xPlat.services', ['ngResource']);
-//})();
